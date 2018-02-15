fdae1d99c224088cd14123bf3f19da5194180d65
Lavrans Grønseth <lavrans.gronseth@gmail.com>
2018-02-15 16:05:54 +0100

    setup webpack and start implementing iota data markedplace publish

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    entry: "./src/app.js",
+    output: {
+        filename: "./bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/
+            }
+        ]
+    }
+};
