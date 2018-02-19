399042e7dd40fec3c062588e5c8ea03a0a6508ad
Odd Kristian Kvarmestol <okknor@gmail.com>
2018-02-19 13:56:28 +0100

    Fix importing the Mam library

--- src/data_publisher.js
+++ src/data_publisher.js
@@ -1,11 +1,8 @@
-
-
 var IOTA = require('iota.lib.js');
-var Mam = require('mam.client.js');
-//import IOTA from "./vendor/iota.min.js";
+require('./vendor/mam.web.js');
 var iota = new IOTA({ provider: 'https://testnet140.tangle.works' });
 
-console.log("mam:",Mam.MAM);
+let Mam;
 
 // Set Varibles
 var debug = true // Set to 'false' to publish data live
@@ -27,81 +24,57 @@ const keyGen = length => {
     return result.join('')
 }
 
-/// experimental
+let mam_initialized = false;
+let mamState;
+let mamKey;
 
-const seed = 'XXMPUFDZTAWNORLGZT9SZXHXXMSINBQVPCJITKOGIIPPUCARZEATSCUBMRXXQTXYRUTXUCBEV9YUMIFJB';
-const start = 3;
-const count = 4;
-const security = 2;
-
-const tree0 = new MerkleTree(seed, start, count, security);
-const tree1 = new MerkleTree(seed, start + count, count, security);
-
-const mam = MAM.create({
-    message: 'WCTC9D9DCDFAEADBNA',
-    merkleTree: tree0,
-    index: index,
-    nextRoot: tree1.root.hash.toString(),
-    channelKey: 'EJTKEICAUGQFUHZNTYMVUDJLFYQAMYUZOJSCDBTXE9CMZGYUVFIHGDVHCLHJCEGDZGXJKJZKQADZBSFEL'
-  });
-
-///////////////////////////////////////
-
-// Initialise MAM State
-let mamState = Mam.init(iota, keyGen(81))
-let mamKey = keyGen(81) // Set initial key
+function init_mam() {
+	Mam = window.Mam;
+	// Initialise MAM State
+	mamState = Mam.init(iota, keyGen(81))
+	mamKey = keyGen(81) // Set initial key
+}
 
 // Publish to tangle
 export const publish = async packet => {
-    // Set channel mode & update key
-    mamState = Mam.changeMode(mamState, 'restricted', mamKey)
-    // Create Trytes
-    var trytes = iota.utils.toTrytes(JSON.stringify(packet))
-    // Get MAM payload
-    var message = Mam.create(mamState, trytes)
-    // Save new mamState
-    mamState = message.state
-    // Attach the payload.
-    await Mam.attach(message.payload, message.address)
-    console.log('Attached Message')
+	if (!mam_initialized) {
+		init_mam();
+	}
+	// Set channel mode & update key
+	mamState = Mam.changeMode(mamState, 'restricted', mamKey)
+	// Create Trytes
+	var trytes = iota.utils.toTrytes(JSON.stringify(packet))
+	// Get MAM payload
+	var message = Mam.create(mamState, trytes)
+	// Save new mamState
+	mamState = message.state
+	// Attach the payload.
+	await Mam.attach(message.payload, message.address)
+	console.log('Attached Message')
 
-    if (!debug) {
-    // Push the MAM root to the demo DB
-    let pushToDemo = await pushKeys(message.root, mamKey)
-    console.log(pushToDemo)
-    // Change MAM key on each loop
-    mamKey = keyGen(81)
-    }
+	if (!debug) {
+		// Push the MAM root to the demo DB
+		let pushToDemo = await pushKeys(message.root, mamKey)
+		console.log(pushToDemo)
+		// Change MAM key on each loop
+		mamKey = keyGen(81)
+	}
 }
 
 // Push keys to market place.
 const pushKeys = async (root, sidekey) => {
-    const packet = {
-    sidekey: sidekey,
-    root: root,
-    time: Date.now()
-    }
-    // Initiate Fetch Call
-    var resp = await fetch(endpoint, {
-    method: 'post',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ id: uuid, packet, sk: secretKey })
-    })
-    return resp.json()
+	const packet = {
+		sidekey: sidekey,
+		root: root,
+		time: Date.now()
+	}
+	// Initiate Fetch Call
+	var resp = await fetch(endpoint, {
+		method: 'post',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({ id: uuid, packet, sk: secretKey })
+	})
+	return resp.json()
 }
-
-// // Emulate data being ingested
-// Array(4)
-//     .fill()
-//     .map((_, i) =>
-//     publish({
-//         time: Date.now(),
-//         // Change below to read actual values!
-//         data: {
-//          temp: "20", 
-
-//         }
-//     })
-//     )
