7d91c0834425f2542180e461d71981f254e252c7
freddysouth <fredd-sormo-1995@hotmail.com>
2018-05-04 14:22:27 +0200

    finish the aggregator script

--- src/aggregator.js
+++ src/aggregator.js
@@ -1,32 +1,27 @@
 //() {} []
 //test packet
-let dummy_packet = {'temperature':24,'humidity':880,
-    'pressure':400,'co2':400,'voc':2};
-
+//let dummy_packet = {'temperature':24,'humidity':880,'pressure':400,'co2':400,'voc':2};
 let index = 0;
-
 let packets = [];
-function append(packet) {
+
+export const appendPacket = (packet) => {
     packets.push(packet);
+    console.log(packets);
 }
 
 function get() {
     let sumList = [0, 0, 0, 0, 0];
     for(let i = index; i < packets.length; i++ ) {
-        sumList[0] += packets[i]['temprature'];
+        sumList[0] += packets[i]['temperature'];
         sumList[1] += packets[i]['humidity'];
         sumList[2] += packets[i]['pressure'];
         sumList[3] += packets[i]['co2'];
         sumList[4] += packets[i]['voc'];
     }
 
-    console.log(sumList);
-    sumList.forEach((e) => {
-        e = e/packets.length - index;
-    });
-    console.log(sumList);
+    sumList.forEach((e, i, a) => a[i] = a[i]/ (packets.length - index));
     index = packets.length;
-    console.log("index: " + index);
+
     return {
         'temperature':sumList[0],
         'humidity': sumList[1],
@@ -34,11 +29,3 @@ function get() {
         'co2': sumList[3],
         'voc': sumList[4]};
 }
-
-append(dummy_packet);
-append(dummy_packet);
-append(dummy_packet);
-append(dummy_packet);
-
-get();
-
