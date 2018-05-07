e0ee116ae052d20618922fd0da39d79e0fc4a54d
freddysouth <fredd-sormo-1995@hotmail.com>
2018-05-07 12:11:22 +0200

    rewrite get to get_aggregate, and restructure it to work with dynamic data sets

--- src/aggregator.js
+++ src/aggregator.js
@@ -1,31 +1,35 @@
 //() {} []
-//test packet
-//let dummy_packet = {'temperature':24,'humidity':880,'pressure':400,'co2':400,'voc':2};
+//packet_blueprint - The expected structure and values of a packet
+//index - The last endpoint for aggregation
+//packets - The list of received packets
+let packet_blueprint = ['temperature', 'humidity','pressure','co2','voc'];
 let index = 0;
 let packets = [];
 
+//
 export const appendPacket = (packet) => {
     packets.push(packet);
-    console.log("Aggregator packets: " + packets);
-}
+};
 
-export const get = () => {
-    let sumList = [0, 0, 0, 0, 0];
-    for(let i = index; i < packets.length; i++ ) {
-        sumList[0] += packets[i]['temperature'];
-        sumList[1] += packets[i]['humidity'];
-        sumList[2] += packets[i]['pressure'];
-        sumList[3] += packets[i]['co2'];
-        sumList[4] += packets[i]['voc'];
+export const get_aggregate = () => {
+    let data_table = [[], [], [], [], []];
+    for(let i = 0; i < packet_blueprint.length; i++) {
+        let count = packets.length - 1;
+        while (true) {
+            if (count < index) break;
+            if (packets[count][packet_blueprint[i]]){
+                data_table[i].push(packets[count][packet_blueprint[i]]);
+            } else break;
+        }
+        count--;
     }
+    let packet = {};
+    data_table.forEach( (table_entry, table_index) => {
+        if(table_entry.length > 0)
+            packet[packet_blueprint[table_index]] = table_entry.reduce((a, b) => a + b, 0) / table_entry.length;
+    });
 
-    sumList.forEach((e, i, a) => a[i] = a[i]/ (packets.length - index));
     index = packets.length;
 
-    return {
-        'temperature':sumList[0],
-        'humidity': sumList[1],
-        'pressure': sumList[2],
-        'co2': sumList[3],
-        'voc': sumList[4]};
-}
+    return packet;
+};
