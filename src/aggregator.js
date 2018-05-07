548a74b43e60c011ac285f70be359ec3b89f1ef5
freddysouth <fredd-sormo-1995@hotmail.com>
2018-05-07 14:37:28 +0200

    rewrite to object oriented process flow

--- src/aggregator.js
+++ src/aggregator.js
@@ -3,37 +3,37 @@
 //index - The last endpoint for aggregation
 //packets - The list of received packets
 let packet_blueprint = ['temperature', 'humidity','pressure','co2','voc'];
-let index = 0;
-let packets = [];
+let data_table = {};
 
-//
-export const appendPacket = (packet) => {
-    packets.push(packet);
+class data_list {
+    constructor(type) {
+        this.type = type;
+        this.list = [];
+    }
+    append(data) {
+        this.list.push(data);
+    }
+    get_average() {
+        if (this.list.length === 0) return null;
+        return (Math.round( (this.list.reduce((a, b) => a + b, 0) /
+            this.list.length) * 100 ) / 100).toString();
+    }
+}
+
+for (let i = 0; i < packet_blueprint.length; i++) {
+    data_table[packet_blueprint[i]] = new data_list(packet_blueprint[i]);
+}
+
+export const append_datapoint = (data, type) => {
+    data_table[type].append(data);
 };
 
-export const get_aggregate = () => {
-    let data_table = [[], [], [], [], []];
-    for(let i = 0; i < packet_blueprint.length; i++) {
-        let count = packets.length - 1;
-        while (true) {
-            if (count < index) break;
-            if (packets[count][packet_blueprint[i]]){
-                console.log("adding",packet_blueprint[i],packets[count][packet_blueprint[i]]);
-                data_table[i].push(packets[count][packet_blueprint[i]]);
-            } else break;
-            count--;
-        }
-
-    }
+export const compose_packet = () => {
     let packet = {};
-    console.log("filled data_table",data_table);
-    data_table.forEach( (table_entry, table_index) => {
-        if(table_entry.length > 0)
-            packet[packet_blueprint[table_index]]
-                = (Math.round( (table_entry.reduce((a, b) => a + b, 0) / table_entry.length ) * 100 ) / 100).toString();
+    packet_blueprint.forEach(e => {
+        if(data_table[e].get_average() != null){
+            packet[e] = data_table[e].get_average();
+        }
     });
-
-    index = packets.length;
-    console.log("packet to be returned by get_aggregate",packet);
     return packet;
 };
