685b96fd5b6b419af0a68d2f30d0d64ed2b33380
freddysouth <fredd-sormo-1995@hotmail.com>
2018-05-07 10:53:09 +0200

    idk

--- src/app.js
+++ src/app.js
@@ -168,7 +168,7 @@ async function start_publishing(device) {
                     if ('transform_data' in options) {
                         data = options.transform_data(data);
                     }
-                    packet[name] = data.value.toString();
+                    packet[name] = data .value.toString();
                     appendPacket(packet);
                 };
                 await enableChannel(update_function, true);
